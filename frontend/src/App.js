a1b2e9b3d819c0a49a819651d3a40f0522eb5754
Julius170 <juliusyoola5@gmail.com>
2022-05-02 00:24:00 +0100

    Added and applied react bootstrap & react router bootstrap for (Amazona)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,28 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Navbar';
-import {LinkContainer} from 'react-router-bootstarp';
+import { Navbar } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import LinkContainer from 'react-router-bootstrap/LinkContainer';
+
+
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <div >
-        <header >
-          <Navbar bg='dark' varient='dark'>
-            <Container>
-              <LinkContainer to="/">
-              <Navbar.Brand>Amazoma</Navbar.Brand>
-              </LinkContainer>
-            </Container>
-          </Navbar>
-        </header>
+     <BrowserRouter>
+       <div >
+         <header >
+            <Navbar bg='dark' varient='dark'>
+              <Container>
+                <LinkContainer to="/">
+                <Navbar.Brand>Amazoma</Navbar.Brand>
+                </LinkContainer>
+              </Container>
+            </Navbar>
+          </header>
+
+
         <main>
           <Routes>
             <Route path='/' element={<HomeScreen />} />
