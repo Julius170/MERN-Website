6dfa136dbb10b674852a8e1d12c03e4daa8fe83d
Julius170 <juliusyoola5@gmail.com>
2022-05-02 16:44:53 +0100

    added increacse and decreace functionality to cart with little bug

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -4,12 +4,25 @@ import { Helmet } from 'react-helmet-async';
 import { Row, Col, ListGroupItem, Button, Card, ListGroup } from 'react-bootstrap';
 import MessageBox from '../components/MessageBox';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default function CartScreen() {
 const { state, dispath: ctxDispatch } = useContext(Store); 
     const {
         cart: { cartItems },
     } = state;
+
+    const updateCardHandler = async (item, quantity) => {
+        const { data } = await axios.get(`http://localhost:5000/api/product/${item._id}`);
+        if (data.countInStock < quantity) {
+            window.alert("Sorry, Product is out of stock");
+            return;
+        }
+        ctxDispatch({
+            type: 'CART_ADD_ITEM', 
+            payload: {...item, quantity },
+        });
+    }
     return (
         <div>
             <Helmet>
@@ -37,18 +50,21 @@ const { state, dispath: ctxDispatch } = useContext(Store);
                                             <Link to={`/product/${item.slug}`}>{item.name}</Link>
                                         </Col>
                                         <Col md={3}>
-                                            <Button variant='light' disable={item.quantity === 1}>
-                                                <i className='fas fa-minus-circle'></i>
+                                            <Button variant='light' disabled={item.quantity === 1}
+                                                onClick = {() => updateCardHandler(item, item.quantity - 1)}>
+                                                <i className='fas fa-minus-circle'>minus</i>
                                             </Button>{' '}
                                             <span>{item.quantity}</span>{' '}
-                                            <Button variant='light' disable={item.quantity === item.countInStock}>
-                                                <i className='fas fa-plus-circle'></i>
+                                            <Button variant='light' 
+                                            onClick = {() => updateCardHandler(item, item.quantity + 1)}
+                                            disabled={item.quantity === item.countInStock}>
+                                                <i className='fas fa-plus-circle'>add</i>
                                             </Button>
                                         </Col>
                                         <Col md={3}>${item.price}</Col>
                                         <Col md={2}>
                                             <Button variant='light'>
-                                                <i className='fas fa-trash'></i>
+                                                <i className='fas fa-trash'>delete</i>
                                             </Button>
                                         </Col>
                                     </Row>
