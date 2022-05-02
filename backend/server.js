118f6b56abfad24609b88fc39a70df8f70455857
Julius170 <juliusyoola5@gmail.com>
2022-05-02 11:49:01 +0100

    Updated cart & add item action

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,17 @@ app.get('/api/product/slug/:slug', (req, res)=> {
     }
 });
 
+
+app.get('/api/product/_id', (req, res)=> {
+    const product = data.products.find(x => x._id === req.params.id);
+    if (product) {
+        res.send(product)
+    } else {
+        res.send(404).send({message: 'Product Not found'});
+    }
+});
+
+
 const port = process.env.port || 5000
 
 app.listen(port, ()=> {
