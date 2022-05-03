c3633afded03d33d165d11559896de5de9f7ffda
Julius170 <juliusyoola5@gmail.com>
2022-05-03 10:51:26 +0100

    added local database with mongodb compass

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,19 @@
 import express  from "express";
 import data from "./data.js";
 import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'; 
 
 const app  = express();
 app.use(cors());
 
-// dotenv.config();
+dotenv.config();
 
-// mongoose.connect(process.env.MONGODB_URI).then(() => {
-//     console.log('connected to db')
-// }).catch(err =>{
-//     console.log(err.message);
-// });
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log('connected to db')
+}).catch(err =>{
+    console.log(err.message);
+});
 
 
 app.get('/api/products', (req, res)=> {
